51976c45fec8d646fd5ae893b7e251770a82662d
Tulsi Mundada <89079161+TulsiMundada@users.noreply.github.com>
2024-09-16 12:53:03 +0530

    Devops Assignment

---
+++ Regression-demo/Pace Prediction/pace-predict.py
@@ -0,0 +1,24 @@
+from flask import Flask , request , jsonify
+import joblib 
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load("final_pace_model.pkl")
+col_names = joblib.load("pace_column_names.pkl")
+
+@app.route('/')
+def hello() -> str:
+    return "Hello, World! from Tulsii"
+
+@app.route('/predict' , methods = ['POST'])
+def predict():
+    user_data = request.json
+    df = pd.DataFrame(user_data)
+    df = df.reindex(columns=col_names)
+    prediction = list(model.predict(df))
+    return jsonify({'prediction': str(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000,host='0.0.0.0')
